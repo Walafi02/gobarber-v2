1b0d6044e6842577dc027fce65af98837d436c96
Walafi02 <walafif@yahoo.com>
2020-06-16 13:05:17 +0000

    Modulo de cadastro de usuario

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import appointments from './appointments.route';
+import users from './users.route';
 
 const router = Router();
 
 router.use('/appointments', appointments);
+router.use('/users', users);
 
 export default router;
