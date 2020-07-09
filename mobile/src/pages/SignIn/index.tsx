967bce034efcc523a7ab45054c1e7dce18498839
Walafi02 <walafif@yahoo.com>
2020-07-09 00:20:46 +0000

    configurando input e button

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
 import logoImg from '../../assets/logo.png';
+
+import Button from '../../compenents/Button';
+import Input from '../../compenents/Input';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça sei logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
